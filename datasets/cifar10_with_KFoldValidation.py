f8be31b7e4396a789288c832820800cb10f04402
Jin <249589575@qq.com>
2021-07-01 21:20:37 +0800

    complete k-fold class

--- datasets/cifar10_with_KFoldValidation.py
+++ datasets/cifar10_with_KFoldValidation.py
@@ -1,7 +1,3 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
-from PIL import Image
 import os
 import cifar10
 
@@ -11,25 +7,34 @@ class cifar10_kfold():
     :param root : 数据的地址
     """
 
-    def __init__(self, root, fold_times, val_index):
+    def __init__(self, root, fold_times):
         super(cifar10_kfold, self).__init__()
         self.root = root.strip("/") + "/"
         self.img_list = os.listdir(root)
-        self.fold_len = int(len(self.img_list) / fold_times)
+        self.per_fold_len = int(len(self.img_list) / fold_times)
+        self.fold_dict = {}
+        self.fold_times = fold_times
+        self.split_data(fold_times)
 
 
     def __len__(self):
         return len(self.img_list)
 
-    def __iter__(self, index):
-        # img_name = self.img_list[index]
-        # img_path = self.root + img_name
-        # img = np.array(Image.open(img_path).convert('RGB'))
-        # img = torch.tensor(img)
-        # img = img.permute([2, 1, 0])
-        # label = int(img_name[0])
-        train_data = cifar10(self.file_list_train)
-        val_data = cifar10(self.file_list_test)
+    def get_data(self, index):
+        train = []
+        val = []
+        for i in range(self.fold_times):
+            if i == index:
+                val = self.fold_dict[i]
+            train += self.fold_dict[i]
 
+        train_data = cifar10(train)
+        val_data = cifar10(val)
         return train_data, val_data
 
+    def split_data(self, k):
+        for i in range(k-1):
+            start = i*self.per_fold_len
+            end = (i+1)*self.per_fold_len
+            self.fold_dict[i] = self.img_list[start:end]
+        self.fold_dict[k-1] = self.img_list[(k-1)*self.per_fold_len:]
